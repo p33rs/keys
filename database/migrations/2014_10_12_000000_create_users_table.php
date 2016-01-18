4ae8c837b3e73af2de2d6b4c8f1a63d26e2d0eb5
p33rs <jon@peers.rip>
2016-01-18 14:50:20 -0500

    adds admin property

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('description');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
